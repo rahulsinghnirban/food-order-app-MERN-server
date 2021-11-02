97a2c81b8f1c370d90ddce44237a0ae9227d5522
rahulsinghnirban <80592933+rahulsinghnirban@users.noreply.github.com>
2021-11-02 23:20:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin: ['http://localhost:3000'],
+        origin: ['http://localhost:3000' ,'https://hungry-jones-2e3e89.netlify.app'],
         credentials: true
     },
 ));
